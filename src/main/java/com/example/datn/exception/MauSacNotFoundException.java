7a7014f2c4ac94b4e2427be50e1304d7624302c9
congnt29N1 <143944102+congnt29N1@users.noreply.github.com>
2023-11-30 23:40:01 +0700

    done mau sac

---
+++ src/main/java/com/example/datn/exception/MauSacNotFoundException.java
@@ -0,0 +1,7 @@
+package com.example.datn.exception;
+
+public class MauSacNotFoundException extends Exception{
+    public MauSacNotFoundException(String massage){
+        super(massage);
+    }
+}
