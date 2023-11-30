c87f6c975dd018079f98d2be24931b6c5bdbcfc0
websiteBanAonamBonique <hieuavph20284@fpt.edu.vn>
2023-11-30 23:37:48 +0700

    chatlieu

---
+++ src/main/java/com/example/datn/exception/ChatLieuNotFoundException.java
@@ -0,0 +1,7 @@
+package com.example.datn.exception;
+
+public class ChatLieuNotFoundException extends Exception {
+    public ChatLieuNotFoundException(String message){
+        super(message);
+    }
+}
