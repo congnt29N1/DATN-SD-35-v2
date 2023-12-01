182b5b0ab0b536a5917af85d1c0fcb72425950cf
ĐỖ PHƯƠNG NAM <namdp1207@gmail.com>
2023-12-01 23:37:18 +0700

    lop lot

---
+++ src/main/java/com/example/datn/controller/loplot/LopLotRestController.java
@@ -0,0 +1,23 @@
+package com.example.datn.controller.loplot;
+
+import com.example.datn.service.LopLotService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.repository.query.Param;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+
+@RestController
+public class LopLotRestController
+{
+    @Autowired
+    LopLotService lopLotService;
+    @Autowired
+    HttpServletRequest request;
+
+    @PostMapping("/admin/loplot/check_name")
+    public String checkDuplicateTen(@Param("id") Integer id , @Param("ten") String ten) {
+        return lopLotService.checkUnique(id, ten) ? "OK" : "Duplicated";
+    }
+}
